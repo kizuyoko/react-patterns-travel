ed639f27324a359df3cce0289d0cb9d05dd76380
kizuyoko <kizuyoko@gmail.com>
2024-03-07 14:58:14 +0100

    Add simple search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,44 @@
 import Accordion from "./components/Accordion/Accordion";
+import SearchableList from "./components/SearchableList/SearchableList";
+
+import savannaImg from './assets/african-savanna.jpg';
+import amazonImg from './assets/amazon-river.jpg';
+import caribbeanImg from './assets/caribbean-beach.jpg';
+import desertImg from './assets/desert-dunes.jpg';
+import forestImg from './assets/forest-waterfall.jpg';
+
+const PLACES = [
+  {
+    id: 'african-savanna',
+    image: savannaImg,
+    title: 'African Savanna',
+    description: 'Experience the beauty of nature.',
+  },
+  {
+    id: 'amazon-river',
+    image: amazonImg,
+    title: 'Amazon River',
+    description: 'Get to know the largest river in the world.',
+  },
+  {
+    id: 'caribbean-beach',
+    image: caribbeanImg,
+    title: 'Caribbean Beach',
+    description: 'Enjoy the sun and the beach.',
+  },
+  {
+    id: 'desert-dunes',
+    image: desertImg,
+    title: 'Desert Dunes',
+    description: 'Discover the desert life.',
+  },
+  {
+    id: 'forest-waterfall',
+    image: forestImg,
+    title: 'Forest Waterfall',
+    description: 'Listen to the sound of the water.',
+  },
+];
 
 function App() {
   return <main>
@@ -28,6 +68,10 @@ function App() {
           </Accordion.Content>
         </Accordion.Item>
       </Accordion>
+    </section>
+    <section>
+      <SearchableList items={PLACES} />
+      <SearchableList items={['item 1', 'item 2']} />
     </section>
   </main>
 }
